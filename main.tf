afbe9ff7216ac6690233c6d2aa083a9bc0f4bd64
sreedhar <psreedharbabu@yahoo.com>
2022-02-22 20:04:03 -0700

    deleted .terraform file

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 module "sreemodule" {
-    source = "./vpcmodule"
+    #source = "./vpcmodule"
+    source = "github.com/sreebabu762/modulevpc?ref=v1.0"
     region = "us-east-1"
     access_key = "AKIASRC4BHTKBY3SRY6I"
     secret_key = "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"
