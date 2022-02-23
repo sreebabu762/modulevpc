f85fc4d55fb05c04e394450a0d0635ec96573f6f
sreedhar <psreedharbabu@yahoo.com>
2022-02-22 19:40:43 -0700

    added tag

--- vpcmodule/main.tf
+++ vpcmodule/main.tf
@@ -1,3 +1,4 @@
+
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
@@ -19,3 +20,4 @@ resource "aws_vpc" "sree_vpc" {
 data "aws_vpc" "sree_vpc" {
      id = aws_vpc.sree_vpc.id
 }
+
