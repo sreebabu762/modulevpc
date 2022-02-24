8d26b3014d08d12d34bb92d043fd6d56607de382
sreebabu762 <99713451+sreebabu762@users.noreply.github.com>
2022-02-23 18:48:05 -0700

    Update main.tf

--- vpcmodule/main.tf
+++ vpcmodule/main.tf
@@ -1,10 +1,3 @@
-
-provider "aws" {
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
-    region = "${var.region}"
-}
-
 resource "aws_vpc" "sree_vpc" {
     cidr_block = "${var.cidr_block}"
     instance_tenancy = "${var.instance_tenancy}"
